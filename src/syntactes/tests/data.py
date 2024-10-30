4a6c2764c37a8658f719ca58e55debc9b7b70f35
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-10-30 18:10:58 +0200

    Adds syntactes modules

---
+++ src/syntactes/tests/data.py
@@ -0,0 +1,26 @@
+from syntactes.grammar import Grammar
+from syntactes.rule import Rule
+from syntactes.token import Token
+
+EOF = Token.eof()
+S = Token("S", False)
+E = Token("E", False)
+T = Token("T", False)
+x = Token("x", True)
+PLUS = Token("+", True)
+
+tokens = {EOF, S, E, T, x, PLUS}
+
+
+# 1. S -> E $
+# 2. E -> T + E
+# 3. E -> T
+# 4. T -> x
+rule_1 = Rule(0, S, E, EOF)
+rule_2 = Rule(1, E, T, PLUS, E)
+rule_3 = Rule(2, E, T)
+rule_4 = Rule(3, T, x)
+
+rules = (rule_1, rule_2, rule_3, rule_4)
+
+grammar = Grammar(rule_1, rules, tokens)
