b02076a39172a41deef20d356bfe7c5e204534b6
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-11-04 13:50:05 +0200

    Uses defined states in generator tests

--- src/syntactes/tests/test_generator.py
+++ src/syntactes/tests/test_generator.py
@@ -2,58 +2,24 @@ from unittest_extensions import TestCase, args
 
 from syntactes._action import Action, ActionType
 from syntactes._item import LR0Item
-from syntactes._state import LR0State
 from syntactes.generator import LR0Generator, SLRGenerator
-from syntactes.tests.data import EOF, PLUS, E, T, grammar, rule_2, rule_3, rule_4, x
-
-
-def state_1():
-    item_1 = LR0Item(grammar.starting_rule, 0)  # S -> . E $
-    item_2 = LR0Item(rule_2, 0)  # E -> . T + E
-    item_3 = LR0Item(rule_3, 0)  # E -> . T
-    item_4 = LR0Item(rule_4, 0)  # T -> . x
-    state = LR0State.from_items({item_1, item_2, item_3, item_4})
-    state.set_number(1)
-    return state
-
-
-def state_2():
-    item_1 = LR0Item(grammar.starting_rule, 1)  # S -> E . $
-    state = LR0State.from_items({item_1})
-    state.set_number(2)
-    return state
-
-
-def state_3():
-    item_1 = LR0Item(rule_2, 1)  # E -> T . + E
-    item_2 = LR0Item(rule_3, 1)  # E -> T .
-    state = LR0State.from_items({item_1, item_2})
-    state.set_number(3)
-    return state
-
-
-def state_4():
-    item_1 = LR0Item(rule_2, 2)  # E -> T + . E
-    item_2 = LR0Item(rule_2, 0)  # E -> . T + E
-    item_3 = LR0Item(rule_3, 0)  # E -> . T
-    item_4 = LR0Item(rule_4, 0)  # T -> . x
-    state = LR0State.from_items({item_1, item_2, item_3, item_4})
-    state.set_number(4)
-    return state
-
-
-def state_5():
-    item_1 = LR0Item(rule_4, 1)  # T -> x .
-    state = LR0State.from_items({item_1})
-    state.set_number(5)
-    return state
-
-
-def state_6():
-    item_1 = LR0Item(rule_2, 3)  # E -> T + E .
-    state = LR0State.from_items({item_1})
-    state.set_number(6)
-    return state
+from syntactes.tests.data import (
+    EOF,
+    PLUS,
+    E,
+    T,
+    grammar,
+    rule_2,
+    rule_3,
+    rule_4,
+    state_1,
+    state_2,
+    state_3,
+    state_4,
+    state_5,
+    state_6,
+    x,
+)
 
 
 def shift(state):
@@ -261,6 +227,7 @@ class TestLR0GeneratorGenerateActions(TestLR0Generator):
     @args(state_6(), x)
     def test_state_6_token_x(self):
         self.assert_state_actions(reduce(rule_2))
+
 
 class TestLR0GeneratorGenerateInitialState(TestLR0Generator):
     def subject(self):
