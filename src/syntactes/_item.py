0500d2907d22a68b2df4038a045754ae0585ccf1
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-12-13 08:55:01 +0200

    Defines lr1 items

--- src/syntactes/_item.py
+++ src/syntactes/_item.py
@@ -59,3 +59,40 @@ class LR0Item:
             return False
 
         return self.rule == other.rule and self.position == other.position
+
+
+class LR1Item(LR0Item):
+    """
+    Item of LR1 parser. Contains rule, current position in rule and lookahead token.
+    Current position is denoted with the dot '.'.
+    """
+
+    def __init__(self, rule: Rule, position: int, lookahead_token: Token) -> None:
+        self.rule = rule
+        self.position = position
+        self.lookahead_token = lookahead_token
+
+    def __repr__(self) -> str:
+        return f"<LR1Item: {self}>"
+
+    def __str__(self) -> str:
+        rhs = [s for s in self.rule.rhs]
+        rhs.insert(self.position, ".")
+        return (
+            f"{self.rule.lhs} -> "
+            + " ".join(map(str, rhs))
+            + f", {self.lookahead_token}"
+        )
+
+    def __hash__(self) -> int:
+        return hash((self.rule, self.position, self.lookahead_token))
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, LR1Item):
+            return False
+
+        return (
+            self.rule == other.rule
+            and self.position == other.position
+            and self.lookahead_token == other.lookahead_token
+        )
